7e20732641d737e18c418b0823e8c815414b7e69
Arghadeep Mazumder <33260290+arghadeep25@users.noreply.github.com>
2024-07-30 15:07:52 +0200

    visualzier added

---
+++ include/utils/image_visualizer.hpp
@@ -0,0 +1,25 @@
+//
+// Created by arghadeep on 30.07.24.
+//
+
+#ifndef IMAGE_STITCHING_IMAGE_VISUALIZER_HPP_
+#define IMAGE_STITCHING_IMAGE_VISUALIZER_HPP_
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <utils/types.hpp>
+
+namespace is::vis {
+/**
+ * Display the image.
+ * @param image The image to display.
+ * @param window_name The window name.
+ * @return void
+ */
+void display(const types::Image &image, const std::string &window_name = "Image") {
+  cv::imshow(window_name, image);
+  cv::waitKey(0);
+}
+}
+
+#endif //IMAGE_STITCHING_IMAGE_VISUALIZER_HPP_
