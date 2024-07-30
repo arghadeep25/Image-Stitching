2193303ae25401245436160c2508f20905c247ef
Arghadeep Mazumder <33260290+arghadeep25@users.noreply.github.com>
2024-07-30 19:51:57 +0200

    image matching sample tested

--- src/image_stitching.cpp
+++ src/image_stitching.cpp
@@ -6,6 +6,7 @@
 #include <utils/scoped_timer.hpp>
 #include <feature_match/match_feature.hpp>
 #include <utils/image_visualizer.hpp>
+#include <image_matching/match_image.hpp>
 
 int main() {
   std::cout << "OpenCV Version: " << CV_VERSION << std::endl;
@@ -25,15 +26,18 @@ int main() {
 //    ScopedTimer timer;
   is::types::ImageFeatures features = feature_extraction.get_features();
   std::cout << "Feature Size: " << features.size() << std::endl;
-  for (size_t idx = 0; idx < features.size(); idx++) {
-    is::types::Image disp_;
-    cv::drawKeypoints(images[idx], features[idx].keypoints, disp_);
-    is::vis::display(disp_, "Image " + std::to_string(idx));
-  }
+//  for (size_t idx = 0; idx < features.size(); idx++) {
+//    is::types::Image disp_;
+//    cv::drawKeypoints(images[idx], features[idx].keypoints, disp_);
+//    is::vis::display(disp_, "Image " + std::to_string(idx));
+//  }
   auto matches = is::feature_match::MatchFeature::match(features);
 //  }
-  is::types::Image disp_img;
-  cv::drawMatches(images[0], features[0].keypoints, images[1], features[1].keypoints, matches, disp_img);
-  is::vis::display(disp_img);
+//  is::types::Image disp_img;
+//  cv::drawMatches(images[0], features[0].keypoints, images[1], features[1].keypoints, matches, disp_img);
+//  is::vis::display(disp_img);
 
+  auto homography = is::match_image::MatchImage::match(features[0].keypoints, features[1].keypoints, matches);
+  is::types::Image stitched_image;
+  cv::drawMatches(images[0], features[0].keypoints, images[1], features[1].keypoints, matches, stitched_image);
 }
