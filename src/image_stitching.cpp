8cd54aa04ce954c6ab92038373147a57a71a8416
Arghadeep Mazumder <33260290+arghadeep25@users.noreply.github.com>
2024-07-30 15:07:01 +0200

    feature extraction and matching tested in main file

--- src/image_stitching.cpp
+++ src/image_stitching.cpp
@@ -4,16 +4,36 @@
 #include <utils/image_reader.hpp>
 #include <utils/types.hpp>
 #include <utils/scoped_timer.hpp>
+#include <feature_match/match_feature.hpp>
+#include <utils/image_visualizer.hpp>
 
 int main() {
-  std::cout << "Hello, OpenCV!" << std::endl;
-  std::string image_path = "../data/001.jpg";
-  cv::Mat image = image_stitching::utils::read_image(image_path);
-  image_stitching::is::ImageFeature feature;
-  image_stitching::feature_extraction::FeatureExtraction feature_extraction;
-  feature_extraction.load_images({image});
-  {
-    ScopedTimer timer;
-    std::vector<image_stitching::is::ImageFeature> features = feature_extraction.get_features();
+  std::cout << "OpenCV Version: " << CV_VERSION << std::endl;
+
+  std::vector<std::string> image_paths;
+  image_paths.emplace_back("../data/001.jpg");
+  image_paths.emplace_back("../data/002.jpg");
+//  image_paths.emplace_back("../data/003.jpg");
+
+  is::types::ImageBatch images = is::utils::read_images(image_paths);
+
+  is::types::ImageFeature feature;
+  is::extract_feature::FeatureExtraction feature_extraction;
+  feature_extraction.load_images(images);
+
+//  {
+//    ScopedTimer timer;
+  is::types::ImageFeatures features = feature_extraction.get_features();
+  std::cout << "Feature Size: " << features.size() << std::endl;
+  for (size_t idx = 0; idx < features.size(); idx++) {
+    is::types::Image disp_;
+    cv::drawKeypoints(images[idx], features[idx].keypoints, disp_);
+    is::vis::display(disp_, "Image " + std::to_string(idx));
   }
+  auto matches = is::feature_match::MatchFeature::match(features);
+//  }
+  is::types::Image disp_img;
+  cv::drawMatches(images[0], features[0].keypoints, images[1], features[1].keypoints, matches, disp_img);
+  is::vis::display(disp_img);
+
 }
