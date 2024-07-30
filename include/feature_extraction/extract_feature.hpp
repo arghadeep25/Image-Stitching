4d20c17a05b103ce5f1d2cbe13f434fc7f3f201e
Arghadeep Mazumder <33260290+arghadeep25@users.noreply.github.com>
2024-07-30 15:06:21 +0200

    threading introduced in get_features func in extract_feature header

--- include/feature_extraction/extract_feature.hpp
+++ include/feature_extraction/extract_feature.hpp
@@ -8,35 +8,69 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <opencv2/features2d.hpp>
+#include <mutex>
+#include <thread>
+#ifdef HAVE_OPENCV_XFEATURES2D
+#include "opencv2/xfeatures2d.hpp"
+#endif
+
 #include <utils/types.hpp>
 #include <vector>
 
-namespace image_stitching::feature_extraction {
+namespace is::extract_feature {
 class FeatureExtraction {
-public:
+ public:
   FeatureExtraction() = default;
 
-public:
+ public:
   ~FeatureExtraction() {
     this->m_images_.clear();
     this->features_.clear();
   }
 
-public:
+ public:
+  /**
+   * Load images in the form of a vector to extract features.
+   * @param images The images to extract features from.
+   * @return void
+   */
   void load_images(const std::vector<cv::Mat> &images) {
     for (const auto &image : images)
       this->m_images_.push_back(image);
   }
 
-public:
-  std::vector<is::ImageFeature> get_features() {
-    for (const auto &image : this->m_images_)
-      this->features_.push_back(this->extract_feature(image));
+ public:
+  /**
+   * Get the extracted features.
+   * @return The extracted features.
+   */
+  inline std::vector<types::ImageFeature> get_features() {
+    std::vector<std::thread> threads;
+    std::vector<types::ImageFeature> local_features;
+    std::mutex local_mutex;
+
+    for (const auto &image : this->m_images_) {
+      threads.emplace_back([&]() {
+        auto feature = this->extract_feature(image);
+        std::lock_guard<std::mutex> lock(local_mutex);
+        local_features.push_back(feature);
+      });
+    }
+
+    for (auto &thread : threads)
+      if (thread.joinable())
+        thread.join();
+
+    {
+      std::lock_guard<std::mutex> lock(this->m_mutex_);
+      this->features_ = std::move(local_features);
+    }
+
     return this->features_;
   }
 
-private:
-  is::ImageFeature extract_feature(const cv::Mat &image) {
+ private:
+  types::ImageFeature extract_feature(const cv::Mat &image) {
     cv::Mat image_copy = image.clone();
 
     if (image_copy.empty())
@@ -45,7 +79,7 @@ private:
     if (image_copy.channels() == 3)
       cv::cvtColor(image_copy, image_copy, cv::COLOR_BGR2GRAY);
 
-    is::ImageFeature feature;
+    types::ImageFeature feature;
     feature.descriptors = cv::Mat();
     feature.keypoints = std::vector<cv::KeyPoint>();
 
@@ -55,10 +89,12 @@ private:
     return feature;
   }
 
-private:
+ private:
   std::vector<cv::Mat> m_images_;
-  std::vector<is::ImageFeature> features_;
+  std::vector<types::ImageFeature> features_;
+  std::mutex m_mutex_;
 };
+
 } // namespace image_stitching::feature_extraction
 
 #endif // IMAGE_STITCHING_EXTRACT_FEATURE_HPP
