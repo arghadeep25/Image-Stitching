9b37a22a08a89b2d35c1851a9abd13e9dc6333df
Arghadeep Mazumder <arghadeep.mazumder@teraki.com>
2024-07-31 10:15:51 +0200

    changed to clang-format

--- include/utils/image_visualizer.hpp
+++ include/utils/image_visualizer.hpp
@@ -16,10 +16,11 @@ namespace is::vis {
  * @param window_name The window name.
  * @return void
  */
-void display(const types::Image &image, const std::string &window_name = "Image") {
+void display(const types::Image &image,
+             const std::string &window_name = "Image") {
   cv::imshow(window_name, image);
   cv::waitKey(0);
 }
-}
+} // namespace is::vis
 
-#endif //IMAGE_STITCHING_IMAGE_VISUALIZER_HPP_
+#endif // IMAGE_STITCHING_IMAGE_VISUALIZER_HPP_
