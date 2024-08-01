fa461122a572992bc966de2ec00fb6edc7aa19a4
Arghadeep Mazumder <33260290+arghadeep25@users.noreply.github.com>
2024-08-01 17:37:06 +0200

    documentation added

--- include/feature_match/match_feature.hpp
+++ include/feature_match/match_feature.hpp
@@ -1,6 +1,11 @@
-//
-// Created by arghadeep on 30.07.24.
-//
+/**
+ * @file match_feature.hpp
+ * @details Feature Matching using Euclidean distance.
+ *          and Lowe's ratio test.
+ * @author Arghadeep Mazumder
+ * @version 0.1.0
+ * @copyright -
+ */
 
 #ifndef IMAGE_STITCHING_MATCH_FEATURE_HPP_
 #define IMAGE_STITCHING_MATCH_FEATURE_HPP_
@@ -12,8 +17,10 @@
 
 namespace is::feature_match {
 /**
- * Feature matching using the Euclidean distance. Lowe's ratio
- * test is used to filter out the false matches.
+ * The purpose of this header is to match the features extracted
+ * using the class is::extract_feature::FeatureExtraction. The
+ * features are matched using the Euclidean distance. Lowe's
+ * ratio test is used to filter out the false matches.
  */
 class MatchFeature {
  public:
@@ -24,11 +31,13 @@ class MatchFeature {
 
  public:
   /**
-   * Match the features of the images.
-   * @param images_features extracted from keypoint descriptors
+   * @brief Match the features of the images.
+   * @param images_features extracted from keypoint descriptors. At
+   *                        least two images are required.
    * @param ratio default value is 0.75
    */
-  static std::vector<cv::DMatch> match(const types::ImageFeatures &images_features, const float &ratio = 0.75f) {
+  static std::vector<cv::DMatch> match(const types::ImageFeatures &images_features,
+                                       const float &ratio = 0.75f) {
     if (images_features.size() < 2)
       throw std::invalid_argument("At least two images are required to match features");
 
@@ -37,7 +46,9 @@ class MatchFeature {
     std::vector<cv::DMatch> good_matches;
 
     for (size_t idx = 0; idx < images_features.size() - 1; idx++)
-      matcher.knnMatch(images_features[idx].descriptors, images_features[idx + 1].descriptors, knn_matches, 2);
+      matcher.knnMatch(images_features[idx].descriptors,
+                       images_features[idx + 1].descriptors,
+                       knn_matches, 2);
 
     for (auto &knn_match : knn_matches)
       if (knn_match[0].distance < ratio * knn_match[1].distance)
