eefbf6be00c502b4fe8f030b1a655a4c7dfc2349
Arghadeep Mazumder <33260290+arghadeep25@users.noreply.github.com>
2024-08-05 12:32:16 +0200

    warping from homography and blending header added

--- include/blending/blend.hpp
+++ include/blending/blend.hpp
@@ -14,6 +14,8 @@
 
 #include <opencv2/opencv.hpp>
 #include <utils/types.hpp>
+#include <featues/features.hpp>
+#include <utils/image_visualizer.hpp>
 
 namespace is::blend {
 /**
@@ -27,252 +29,241 @@ class ImageBlending {
   ~ImageBlending() = default;
 
  public:
-  /**
-   * Blend two images using the given homography matrix.
-   * @param image1 The first image.
-   * @param image2 The second image.
-   * @param homography The homography matrix.
-   * @todo Implement the advanced exposure compensation method
-   * @todo Implement handling multiple images
-   * @return The blended image.
-   */
-  cv::Mat blend(const cv::Mat &image1, const cv::Mat &image2, const cv::Mat &homography) {
-    return warp_images(image1, image2, homography);
-//    cv::Size size = {image1.cols + image2.cols, image1.rows};
-//    // 1. Warp image
-//    auto wrapped_image1 = warp_image(image1, homography, size);
-//    auto wrapped_image2 = warp_image(image2, cv::Mat::eye(3, 3, CV_64F), size);
-//
-//    // 2. Exposure Compensation
-//    cv::Mat mask = cv::Mat::zeros(size, CV_8U);
-//    cv::rectangle(mask, cv::Rect(0, 0, size.width, size.height), cv::Scalar(255), -1);
-//    auto exposure_compensated_image = exposure_compensation(wrapped_image1, wrapped_image2, mask);
-//
-//    // 3. Find Optimal Seam
-//    auto optimal_seam = find_optimal_seam(wrapped_image1, wrapped_image2);
-//
-//    // 4. Multi-band blending
-//    auto blended_image = multi_band_blending(wrapped_image1, wrapped_image2, mask);
-//    return blended_image;
-  };
+  void warp_images(const cv::Mat &src_img, const cv::Mat &dst_img) {
+    std::cout << "src_img_typeL " << src_img.type() << std::endl;
+    std::cout << "dst_img_typeL " << dst_img.type() << std::endl;
 
- private:
-  /**
-   * @brief Warp images using the given homography matrix
-   * @param image1
-   * @param image2
-   * @param H
-   * @return
-   */
-  cv::Mat warp_images(const cv::Mat &image1, const cv::Mat &image2, const cv::Mat &H) {
-    // Get image dimensions
-    int h1 = image1.rows;
-    int w1 = image1.cols;
-    int h2 = image2.rows;
-    int w2 = image2.cols;
+    is::features::Features features;
+    cv::Mat homography = features.compute_homography(src_img, dst_img);
 
-    // Points for the transformed image boundaries
-    std::vector<cv::Point2f> pts1 = {cv::Point2f(0, 0), cv::Point2f(0, h1),
-                                     cv::Point2f(w1, h1), cv::Point2f(w1, 0)};
-    std::vector<cv::Point2f> pts2 = {cv::Point2f(0, 0), cv::Point2f(0, h2),
-                                     cv::Point2f(w2, h2), cv::Point2f(w2, 0)};
+    // Height and Width of the source image
+    int src_height = src_img.rows;
+    int src_width = src_img.cols;
 
-    // Transform points to the new coordinates using the homography matrix
-    std::vector<cv::Point2f> pts2_transformed;
-    cv::perspectiveTransform(pts2, pts2_transformed, H);
+    // Height and Width of the destination image
+    int dst_height = dst_img.rows;
+    int dst_width = dst_img.cols;
 
-    // Combine the points from both images
-    std::vector<cv::Point2f> all_points = pts1;
-    all_points.insert(all_points.end(), pts2_transformed.begin(), pts2_transformed.end());
+    // Extract the corners of the source image
+    std::vector<cv::Point2f> src_corners = {cv::Point2f(0, 0),
+                                            cv::Point2f(0, static_cast<float>(src_height)),
+                                            cv::Point2f(static_cast<float>(src_width), static_cast<float>(src_height)),
+                                            cv::Point2f(static_cast<float>(src_width), 0)};
 
-    // Get the bounding box of the combined image
-    cv::Rect bbox = cv::boundingRect(all_points);
+    // Extract the corners of the destination image
+    std::vector<cv::Point2f> dst_corners = {cv::Point2f(0, 0),
+                                            cv::Point2f(0, static_cast<float>(dst_height)),
+                                            cv::Point2f(static_cast<float>(dst_width), static_cast<float>(dst_height)),
+                                            cv::Point2f(static_cast<float>(dst_width), 0)};
 
-    // Calculate the translation matrix
-    cv::Mat translation_matrix = (cv::Mat_<double>(3, 3) << 1, 0, -bbox.x, 0, 1, -bbox.y, 0, 0, 1);
+    // Apply perspective transformation to the source image
+    std::vector<cv::Point2f> src_warped_corners;
+    cv::perspectiveTransform(src_corners, src_warped_corners, homography);
 
-    // Warp the first image
-    cv::Mat warped_image1;
-    cv::warpPerspective(image1, warped_image1, translation_matrix * H, bbox.size());
+    std::vector<cv::Point2f> dst_corners_warped = src_warped_corners;
+    dst_corners_warped.insert(dst_corners_warped.end(), dst_corners.begin(), dst_corners.end());
 
-    // Warp the second image
-    cv::Mat warped_image2;
-    cv::warpPerspective(image2, warped_image2, translation_matrix, bbox.size());
+    // Extracting Min and Max points
+    cv::Point2f min_point, max_point;
+    auto min_max_coords = this->min_max_points(dst_corners_warped);
+    min_point = min_max_coords.first;
+    max_point = min_max_coords.second;
 
-    // Create a mask for the second image
-    cv::Mat mask2 = (warped_image2 > 0);
-    cv::cvtColor(mask2, mask2, cv::COLOR_BGR2GRAY);
+    cv::Point2f translation(-min_point.x, -min_point.y);
+    std::string side;
+    int width_pano, height_pano = static_cast<int>(max_point.y - min_point.x);
 
-    // Blend the two images together
-    cv::Mat result = warped_image1.clone();
-    warped_image2.copyTo(result, mask2);
-
-    return result;
-  }
-
- private:
-  /**
-   * @brief Warp image using the given homography matrix
-   * @param image
-   * @param homography
-   * @return
-   */
-  cv::Mat warp_image(const cv::Mat &image, const cv::Mat &homography,
-                     const cv::Size &size) {
-    cv::Mat warped_image;
-    cv::warpPerspective(image, warped_image, homography, size);
-    return warped_image;
-  };
-
- private:
-  void warpMultiImages() {};
-
- private:
-  /**
-   * Exposure compensation basic implementation
-   * @todo replace with advanced method of exposure compensation
-   * @param image1
-   * @param image2
-   * @param mask
-   * @return Exposure compensated image
-   */
-  cv::Mat exposure_compensation(const cv::Mat &image1, const cv::Mat &image2,
-                                const cv::Mat &mask, const std::string &type = "mean") {
-    cv::Mat result = image1.clone();
-    cv::Scalar mean1 = cv::mean(image1, mask);
-    cv::Scalar mean2 = cv::mean(image2, mask);
-
-    if (type == "mean") {
-      cv::Scalar meanDiff = mean2 - mean1;
-      result.convertTo(result, -1, 1, -meanDiff[0]);
-    } else if (type == "gain") {
-      cv::Scalar gain = mean2 / mean1;
-      result.convertTo(result, -1, gain[0]);
+    if (dst_corners_warped[0].x < 0) {
+      width_pano = dst_width + static_cast<int>(translation.x);
+      side = "left";
     } else {
-      std::cerr << "Error: Invalid exposure compensation type." << std::endl;
+      width_pano = static_cast<int>(src_warped_corners[3].x);
+      side = "right";
     }
-    return result;
-  };
 
- private:
-  /**
-   * Find optimal seam between two images using absolute difference between the images
-   * @param image1
-   * @param image2
-   * @return Gray Scale Optimal Seam
-   */
-  cv::Mat find_optimal_seam(const cv::Mat &image1, const cv::Mat &image2) {
-    cv::Mat diff, gray_diff;
-    cv::absdiff(image1, image2, diff);
-    cv::cvtColor(diff, gray_diff, cv::COLOR_BGR2GRAY);
-    return gray_diff;
+    // Translation Matrix
+    cv::Mat Ht = cv::Mat::eye(3, 3, homography.type());
+    Ht.at<double>(0, 2) = translation.x;
+    Ht.at<double>(1, 2) = translation.y;
+
+    cv::Mat src_img_warped;
+    cv::warpPerspective(src_img, src_img_warped, Ht * homography, cv::Size(width_pano, height_pano));
+
+    // Resize the destination image
+    cv::Mat dst_img_rz = cv::Mat::zeros(cv::Size(width_pano, height_pano), dst_img.type());
+    if (side == "left") {
+      dst_img_rz(cv::Rect(static_cast<int>(translation.x),
+                          static_cast<int>(translation.y),
+                          dst_width, src_height)) = dst_img;
+    } else {
+      dst_img_rz(cv::Rect(0, static_cast<int>(translation.y),
+                          dst_width, src_height)) = dst_img;
+    }
+
+    // Panorama Blending
+    auto [pano, nonblend, leftside, rightside] =
+        this->panoramaBlending(dst_img_rz, src_img_warped, dst_width, side, true);
+
+    pano = crop(pano, dst_height, dst_corners_warped);
+    is::vis::display(pano, "Pano Image");
+//    test(dst_img_rz, src_img_warped, dst_width, side, true);
+    is::vis::display(src_img_warped, "Warped Image");
+
   }
 
  private:
-  /**
-   * Laplacian pyramid implementation
-   * @param image
-   * @param levels
-   * @return Laplacian pyramid
-   */
-  std::vector<cv::Mat> laplacian_pyramid(const cv::Mat &image,
-                                         const int &levels) {
-    std::vector<cv::Mat> pyramid;
-    cv::Mat current = image.clone();
-    for (int i = 0; i < levels; i++) {
-      cv::Mat down, up;
-      cv::pyrDown(current, down);
-      cv::pyrUp(down, up, current.size());
-      cv::Mat laplacian = current - up;
-      pyramid.push_back(laplacian);
-      current = down;
+  std::pair<cv::Point2f, cv::Point2f> min_max_points(const std::vector<cv::Point2f> &points) {
+    cv::Point2f min_point, max_point;
+    float min_x = std::numeric_limits<float>::max();
+    float min_y = std::numeric_limits<float>::max();
+    float max_x = std::numeric_limits<float>::lowest();
+    float max_y = std::numeric_limits<float>::lowest();
+
+    for (const auto &pt : points) {
+      min_x = std::min(min_x, pt.x);
+      min_y = std::min(min_y, pt.y);
+      max_x = std::max(max_x, pt.x);
+      max_y = std::max(max_y, pt.y);
     }
-    pyramid.push_back(current);
-    return pyramid;
+
+    min_point = {min_x, min_y};
+    max_point = {max_x, max_y};
+
+    return {min_point, max_point};
   }
 
  private:
-  /**
-   * Multi-band blending implementation
-   * @param image1
-   * @param image2
-   * @param mask
-   * @param levels
-   * @return
-   */
-  cv::Mat multi_band_blending(const cv::Mat &image1,
-                              const cv::Mat &image2,
-                              const cv::Mat &mask,
-                              const int &levels = 5) {
-    cv::Mat output;
-    // Building Laplacian pyramid for the images
-    auto laplacian_pyramid1 = laplacian_pyramid(image1, levels);
-    auto laplacian_pyramid2 = laplacian_pyramid(image2, levels);
-
-    // Laplacian pyramid for the mask
-    std::vector<cv::Mat> mask_pyramid;
-
-    // Building Gaussian pyramid for the mask
-    cv::Mat curr_mask = mask.clone();
-    for (size_t i = 0; i < levels; i++) {
-      cv::Mat down;
-      cv::pyrDown(curr_mask, down);
-      mask_pyramid.push_back(curr_mask);
-      curr_mask = down;
-    }
-    mask_pyramid.push_back(curr_mask);
-
-    // Blending Pyramids
-    std::vector<cv::Mat> blended_pyramid;
-    for (size_t i = 0; i < levels; i++) {
-      cv::Mat blended;
-      // Ensure that the mask has the same number of channels as the Laplacian pyramids
-      cv::Mat mask_ = mask_pyramid[i];  // Create a non-const copy
-      if (mask_.channels() == 1 && laplacian_pyramid1[i].channels() == 3) {
-        std::vector<cv::Mat> mask_channels(3);
-        for (int c = 0; c < 3; ++c) {
-          mask_channels[c] = mask_;  // Copy single-channel mask to each channel
+  cv::Mat blend_mask(const int &height,
+                     const int &width,
+                     const int &barrier,
+                     const int &smoothing_window,
+                     bool left_biased) {
+    assert(barrier < width);
+    cv::Mat mask = cv::Mat::zeros(height, width, CV_32F);
+    std::cout << "mask type: " << mask.type() << std::endl;
+    int offset = smoothing_window / 2;
+    try {
+      if (left_biased) {
+        for (int i = 0; i < height; ++i) {
+          for (int j = barrier - offset; j <= barrier + offset; ++j) {
+            mask.at<float>(i, j) = 1 - (float) (j - (barrier - offset)) / (2 * offset + 1);
+          }
         }
-        cv::merge(mask_channels, mask_);
-      }
-      // Multiply the mask with the first Laplacian pyramid image
-      cv::multiply(mask_, laplacian_pyramid1[i], blended);
-      // Multiply the inverse mask with the second Laplacian pyramid image
-      cv::Mat temp;
-      cv::Mat inverted_mask = cv::Scalar::all(1.0) - mask_;
-      cv::multiply(inverted_mask, laplacian_pyramid2[i], temp);
-      // Add the two results together
-      blended += temp;
-      blended_pyramid.push_back(blended);
-    }
-    // Reconstructing the image from the blended pyramid
-    cv::Mat current = blended_pyramid.back();
-
-    for (int i = levels - 1; i >= 0; i--) {
-      cv::Mat up;
-      // Up-sample the image; doubling the size
-      if (!current.empty()) {
-        cv::pyrUp(current, up);
+        mask.colRange(0, barrier - offset).setTo(1);
       } else {
-        std::cerr << "Error: Current matrix is empty before pyrUp." << std::endl;
-        return cv::Mat(); // Return empty matrix on failure
-      }
-      // Expected size after up-sampling
-      cv::Size expected_size = blended_pyramid[i].size();
-      // If the up-sampled size does not match the expected size, adjust manually
-      if (up.size() != expected_size) {
-        if (!up.empty() && expected_size.width > 0 && expected_size.height > 0) {
-          cv::resize(up, up, expected_size);
-        } else {
-          std::cerr << "Error: Upsampled matrix is empty or expected size is invalid." << std::endl;
-          return cv::Mat(); // Return empty matrix on failure
+        for (int i = 0; i < height; ++i) {
+          for (int j = barrier - offset; j <= barrier + offset; ++j) {
+            mask.at<float>(i, j) = (float) (j - (barrier - offset)) / (2 * offset + 1);
+          }
         }
+        mask.colRange(barrier + offset, width).setTo(1);
       }
-      // Add the upsampled image to the corresponding level of the blended pyramid
-      current = up + blended_pyramid[i];
+    } catch (const std::exception &) {
+      if (left_biased) {
+        for (int i = 0; i < height; ++i) {
+          for (int j = barrier - offset; j < barrier + offset; ++j) {
+            mask.at<float>(i, j) = 1 - (float) (j - (barrier - offset)) / (2 * offset);
+          }
+        }
+        mask.colRange(0, barrier - offset).setTo(1);
+      } else {
+        for (int i = 0; i < height; ++i) {
+          for (int j = barrier - offset; j < barrier + offset; ++j) {
+            mask.at<float>(i, j) = (float) (j - (barrier - offset)) / (2 * offset);
+          }
+        }
+        mask.colRange(barrier + offset, width).setTo(1);
+      }
     }
-    output = current;
-    return output;
+
+    cv::Mat mask_3channel;
+    std::vector<cv::Mat> channels = {mask, mask, mask};
+    merge(channels, mask_3channel);
+    mask_3channel.convertTo(mask_3channel, CV_8U);
+    cv::normalize(mask_3channel, mask_3channel, 0, 1, cv::NORM_MINMAX);
+    return mask_3channel;
+  }
+
+ private:
+  std::tuple<cv::Mat, cv::Mat, cv::Mat, cv::Mat> panoramaBlending(const cv::Mat &dst_img_rz,
+                                                                  const cv::Mat &src_img_warped,
+                                                                  int width_dst,
+                                                                  const std::string &side,
+                                                                  bool showstep = false) {
+    int h = dst_img_rz.rows;
+    int w = dst_img_rz.cols;
+    int smoothing_window = width_dst / 8;
+    int barrier = width_dst - smoothing_window / 2;
+
+    cv::Mat mask1 = blend_mask(h, w, barrier, smoothing_window, true);
+    cv::Mat mask2 = blend_mask(h, w, barrier, smoothing_window, false);
+
+    std::cout << "mask1 type: " << mask1.type() << std::endl;
+    std::cout << "mask2 type: " << mask2.type() << std::endl;
+
+    cv::Mat nonblend, leftside, rightside;
+    cv::Mat dst_img_rz_copy = dst_img_rz.clone();
+    cv::Mat src_img_warped_copy = src_img_warped.clone();
+
+    std::cout << "dst_img_rz_copy type: " << dst_img_rz_copy.type() << std::endl;
+    std::cout << "src_img_warped_copy type: " << src_img_warped_copy.type() << std::endl;
+
+
+    // Asserting both the images and masks of same type
+    mask1.convertTo(mask1, dst_img_rz_copy.type());
+    mask2.convertTo(mask2, src_img_warped_copy.type());
+
+    std::cout << "mask1 type: " << mask1.type() << std::endl;
+    std::cout << "mask2 type: " << mask2.type() << std::endl;
+
+    if (side == "left") {
+      cv::flip(dst_img_rz_copy, dst_img_rz_copy, 1);
+      cv::flip(src_img_warped_copy, src_img_warped_copy, 1);
+
+      dst_img_rz_copy = dst_img_rz_copy.mul(mask1);
+      src_img_warped_copy = src_img_warped_copy.mul(mask2);
+
+      cv::Mat pano = src_img_warped_copy + dst_img_rz_copy;
+      cv::flip(pano, pano, 1);
+      if (showstep) {
+        leftside = src_img_warped_copy.clone();
+        rightside = dst_img_rz_copy.clone();
+        cv::flip(leftside, leftside, 1);
+        cv::flip(rightside, rightside, 1);
+      }
+      return {pano, nonblend, leftside, rightside};
+    } else {
+      dst_img_rz_copy = dst_img_rz_copy.mul(mask1);
+      src_img_warped_copy = src_img_warped_copy.mul(mask2);
+      cv::Mat pano = src_img_warped_copy + dst_img_rz_copy;
+      if (showstep) {
+        leftside = dst_img_rz_copy.clone();
+        rightside = src_img_warped_copy.clone();
+      }
+      return {pano, nonblend, leftside, rightside};
+    }
+  }
+
+ private:
+  cv::Mat crop(const cv::Mat &panorama, const int &height, const std::vector<cv::Point2f> &corners) {
+    cv::Point2f min_point, max_point;
+    auto min_max_points = this->min_max_points(corners);
+    min_point = min_max_points.first;
+    max_point = min_max_points.second;
+
+    cv::Point2f translation(-min_point.x, -min_point.y);
+    int n = std::abs(-corners[1].x + corners[0].x);
+    cv::Mat cropped_panorama;
+    if (corners[0].x < 0) {
+      cv::Rect roi(n, static_cast<int>(translation.y),
+                   static_cast<int>(panorama.cols - n), height);
+      cropped_panorama = panorama(roi);
+    } else {
+      cv::Rect roi(0, static_cast<int>(translation.y),
+                   (int) std::min(corners[2].x, corners[3].x), height);
+      cropped_panorama = panorama(roi);
+    }
+    return cropped_panorama;
   }
 };
 }// namespace is::blend
