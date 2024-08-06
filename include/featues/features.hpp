d9e7046413ec760c9f219d730b9c56a71add63c7
Arghadeep Mazumder <arghadeep.mazumder@teraki.com>
2024-08-06 15:56:56 +0200

    edge cases handled and documentation added in features header

--- include/featues/features.hpp
+++ include/featues/features.hpp
@@ -1,17 +1,24 @@
-//
-// Created by arghadeep on 04.08.24.
-//
+/**
+ * @file features.hpp
+ * @details Extracting features and computing Homography.
+ * @author Arghadeep Mazumder
+ * @version 0.1.0
+ * @copyright -
+ */
 
 #ifndef IMAGE_STITCHING__FEATURES_HPP_
 #define IMAGE_STITCHING__FEATURES_HPP_
 
-#include <vector>
 #include <iostream>
-#include <opencv2/opencv.hpp>
 #include <opencv2/features2d.hpp>
+#include <opencv2/opencv.hpp>
 #include <utils/types.hpp>
+#include <vector>
 
 namespace is::features {
+/**
+ * Class to extract features from the image and compute homography.
+ */
 class Features {
   struct FeaturesParameters {
     bool visualization = false;
@@ -21,13 +28,19 @@ class Features {
     double confidence = 0.995;
   };
 
- public:
+public:
   Features() = default;
 
- public:
+public:
   ~Features() = default;
 
- public:
+public:
+  /**
+   * Compute the homography matrix using RANSAC.
+   * @param src_img The source image.
+   * @param dst_img The destination image.
+   * @return The homography matrix.
+   */
   cv::Mat compute_homography(const cv::Mat &src_img, const cv::Mat &dst_img) {
     if (src_img.empty() || dst_img.empty())
       throw std::runtime_error("Image is empty");
@@ -35,8 +48,11 @@ class Features {
     types::ImageFeature src_features = extract_features(src_img);
     types::ImageFeature dst_features = extract_features(dst_img);
 
-    std::vector<cv::DMatch> matches = match_features(src_features.descriptors,
-                                                     dst_features.descriptors);
+    if (src_features.keypoints.empty() || dst_features.keypoints.empty())
+      throw std::runtime_error("Keypoints are empty");
+
+    std::vector<cv::DMatch> matches =
+        match_features(src_features.descriptors, dst_features.descriptors);
 
     std::vector<cv::Point2f> points1, points2;
     for (auto match : matches) {
@@ -44,24 +60,28 @@ class Features {
       points2.push_back(dst_features.keypoints[match.trainIdx].pt);
     }
 
+    if (points1.size() < 4 || points2.size() < 4)
+      throw std::runtime_error("Not enough points to compute homography");
+
     if (params.visualization)
       this->draw_matches(src_img, src_features, dst_img, dst_features, matches);
 
     std::vector<uchar> inliers_mask;
 
-    cv::Mat homography = cv::findHomography(points1,
-                                            points2,
-                                            cv::RANSAC,
-                                            params.rejection_threshold,
-                                            inliers_mask,
-                                            params.max_iterations,
-                                            params.confidence);
-    std::cout << "Homography matrix: \n" << homography << std::endl;
-    return homography;
+    cv::Mat homography = cv::findHomography(
+        points1, points2, cv::RANSAC, params.rejection_threshold, inliers_mask,
+        params.max_iterations, params.confidence);
 
+    std::cout << "Homography Matrix: \n" << homography << std::endl;
+    return homography;
   }
 
- private:
+private:
+  /**
+   * Extract features from the image.
+   * @param img The image.
+   * @return The image features.
+   */
   types::ImageFeature extract_features(const cv::Mat &img) {
     cv::Mat img_clone = img.clone();
 
@@ -76,10 +96,20 @@ class Features {
 
     cv::Ptr<cv::Feature2D> sift = cv::SIFT::create();
     sift->detectAndCompute(img_clone, cv::noArray(), keypoints, descriptors);
+
+    if (keypoints.empty() || descriptors.empty())
+      throw std::runtime_error("Keypoints or descriptors are empty");
+
     return {keypoints, descriptors};
   }
 
- private:
+private:
+  /**
+   * Match the features of the images.
+   * @param src_features The source image features.
+   * @param dst_features The destination image features.
+   * @return The matched features.
+   */
   std::vector<cv::DMatch> match_features(const cv::Mat &src_features,
                                          const cv::Mat &dst_features) {
     if (src_features.empty() || dst_features.empty())
@@ -97,32 +127,50 @@ class Features {
       throw std::runtime_error("Invalid matcher type");
     }
 
+    if (matches.empty())
+      throw std::runtime_error("Matches are empty");
+
     std::vector<cv::DMatch> good_matches;
     for (auto &match : matches)
       if (match[0].distance < 0.75 * match[1].distance)
         good_matches.push_back(match[0]);
 
+    if (good_matches.empty())
+      throw std::runtime_error("Good matches are empty");
+
     return good_matches;
   }
 
- private:
+private:
+  /**
+   * Draw the matches between the images.
+   * @param src_img The source image.
+   * @param src_features The source image features.
+   * @param dst_img The destination image.
+   * @param dst_features The destination image features.
+   * @param matches The matched features.
+   */
   void draw_matches(const cv::Mat &src_img,
                     const types::ImageFeature &src_features,
                     const cv::Mat &dst_img,
                     const types::ImageFeature &dst_features,
                     const std::vector<cv::DMatch> &matches) {
+    if (src_img.empty() || dst_img.empty())
+      throw std::runtime_error("Image is empty for visualization");
+    if (src_features.keypoints.empty() || dst_features.keypoints.empty())
+      throw std::runtime_error("Keypoints are empty for visualization");
+
     cv::Mat img_matches;
-    cv::drawMatches(src_img, src_features.keypoints,
-                    dst_img, dst_features.keypoints,
-                    matches, img_matches);
+
+    cv::drawMatches(src_img, src_features.keypoints, dst_img,
+                    dst_features.keypoints, matches, img_matches);
     cv::imshow("Matches", img_matches);
     cv::waitKey(0);
   }
 
- public:
+public:
   FeaturesParameters params;
 };
 } // namespace is::features
 
-
-#endif //IMAGE_STITCHING__FEATURES_HPP_
+#endif // IMAGE_STITCHING__FEATURES_HPP_
