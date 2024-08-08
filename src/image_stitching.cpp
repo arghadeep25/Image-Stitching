68446fe61da39623c1e65a43f63c6a456b5fcbb3
Arghadeep Mazumder <arghadeep.mazumder@teraki.com>
2024-08-08 15:32:32 +0200

    sample example added

--- src/image_stitching.cpp
+++ src/image_stitching.cpp
@@ -1,72 +1,19 @@
-#include <blending/blend.hpp>
-#include <feature_extraction/extract_feature.hpp>
-#include <feature_match/match_feature.hpp>
-#include <image_matching/match_image.hpp>
+
 #include <iostream>
 #include <opencv2/opencv.hpp>
-#include <utils/image_reader.hpp>
+#include <stitch/stitch.hpp>
 #include <utils/image_visualizer.hpp>
 #include <utils/scoped_timer.hpp>
-#include <utils/types.hpp>
-#include <features/features.hpp>
 
 int main() {
   std::cout << "OpenCV Version: " << CV_VERSION << std::endl;
-  auto image1 = is::utils::read_image("../data/001.jpg");
-  auto image2 = is::utils::read_image("../data/002.jpg");
-
-//  cv::Mat new_image = cv::Mat::zeros(image1.size(), image1.type());
-//  auto roi = cv::Rect(0, 0, 400, 400);
-//  image1(roi).copyTo(new_image(roi));
-//  std::cout << "new_image pixel value: " << new_image.at<cv::Vec3b>(100, 100) << std::endl;
-//  is::vis::display(new_image, "New Image");
-
-//  is::features::Features feature;
-//  feature.params.rejection_threshold = 3.0;
-//  auto homography = feature.compute_homography(image1, image2);
-  is::blend::ImageBlending blending;
-  blending.warp_images(image1, image2);
-  // print homography
-//  std::vector<std::string> image_paths;
-//  image_paths.emplace_back("../data/001.jpg");
-//  image_paths.emplace_back("../data/002.jpg");
-////  image_paths.emplace_back("../data/003.jpg");
-//
-//  is::types::ImageBatch images = is::utils::read_images(image_paths);
-//
-//  is::types::ImageFeature feature;
-//  is::extract_feature::FeatureExtraction feature_extraction;
-//  feature_extraction.load_images(images);
-//
-//  {
-//    ScopedTimer timer;
-//    is::types::ImageFeatures features = feature_extraction.get_features();
-//    std::cout << "Feature Size: " << features.size() << std::endl;
-//    //  for (size_t idx = 0; idx < features.size(); idx++) {
-//    //    is::types::Image disp_;
-//    //    cv::drawKeypoints(images[idx], features[idx].keypoints, disp_);
-//    //    is::vis::display(disp_, "Image " + std::to_string(idx));
-//    //  }
-//    auto matched_features = is::feature_match::MatchFeature::match(features);
-//    //  }
-//    //  is::types::Image disp_img;
-//    //  cv::drawMatches(images[0], features[0].keypoints, images[1],
-//    //  features[1].keypoints, matches, disp_img); is::vis::display(disp_img);
-//    is::match_image::MatchImage match_image;
-//    //    auto verified_matches = match_image.match(
-//    //        features[0].keypoints, features[1].keypoints, matched_features);
-//
-//    auto homography = is::match_image::MatchImage::compute_homography(
-//        features[0].keypoints, features[1].keypoints, matched_features);
-//
-//    auto verified_matches = match_image.match(
-//        features[0].keypoints, features[1].keypoints, matched_features);
-//    is::types::Image stitched_image;
-//    cv::drawMatches(images[0], features[0].keypoints, images[1],
-//                    features[1].keypoints, verified_matches, stitched_image);
-//    //        is::vis::display(stitched_image, "Stitched Image");
-//    is::blend::ImageBlending blending;
-//    auto blended_image = blending.blend(images[0], images[1], homography);
-//    is::vis::display(blended_image, "Blended Image");
-//  }
+  cv::Mat pano;
+  {
+    ScopedTimer timer;
+    is::stitch::Stitch stitch;
+    stitch.load_images("../data/berlin", false);
+    pano = stitch.stitch();
+  }
+  is::vis::display(pano, "Panorama");
+  cv::imwrite("../results/berlin.jpg", pano);
 }
