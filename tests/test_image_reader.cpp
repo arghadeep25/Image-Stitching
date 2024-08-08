550e40efe4a0b69d31dac722067d4153f8170cba
Arghadeep Mazumder <arghadeep.mazumder@teraki.com>
2024-08-08 16:50:18 +0200

    path changed to relative for local and ci/cd pipeline

--- tests/test_image_reader.cpp
+++ tests/test_image_reader.cpp
@@ -5,12 +5,13 @@
 #include <utils/types.hpp>
 
 TEST(OpenCVTest, CanLoadImage) {
-  is::types::Image image = is::utils::read_image("../../data/berlin/001.jpg");
-  if (image.empty()) std::cout << "Image is empty" << std::endl;
+  std::string image_path =
+      std::string(PROJECT_SOURCE_DIR) + "/data/berlin/001.jpg";
+  is::types::Image image = is::utils::read_image(image_path);
+  if (image.empty())
+    std::cout << "Image is empty" << std::endl;
   ASSERT_FALSE(image.empty());
 }
-
-
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
