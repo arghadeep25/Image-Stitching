a5bf3939c4661a78644955774856cc5f3fbe9416
Arghadeep Mazumder <arghadeep.mazumder@teraki.com>
2024-08-08 15:32:09 +0200

    stitch header updated with data types

--- include/stitch/stitch.hpp
+++ include/stitch/stitch.hpp
@@ -1,6 +1,7 @@
 /**
- * @file blend.hpp
- * @details Image blending using multi-band blending.
+ * @file stitch.hpp
+ * @details Stitch multiple images together to create a panorama. The images are
+ * loaded from the given path. The images are stitched using image blending.
  * @author Arghadeep Mazumder
  * @version 0.1.0
  * @copyright -
@@ -14,10 +15,21 @@
 #include <iostream>
 #include <list>
 #include <opencv2/opencv.hpp>
+#include <utils/image_reader.hpp>
 #include <utils/types.hpp>
 #include <vector>
 
 namespace is::stitch {
+/**
+ * @brief Class to stitch multiple images together to create a panorama.
+ * The images are loaded from the given path. The images are stitched
+ * using image blending.
+ * @details First the images are loaded from the given path. The images are
+ * resized if the resize flag is set to true. The stitching process is done
+ * in two steps. First, the left images are stitched together. Then, the right
+ * images are stitched together. Finally, the left and right panoramas are
+ * stitched together to create the final panorama.
+ */
 class Stitch {
 public:
   Stitch() = default;
@@ -31,6 +43,12 @@ public:
   };
 
 public:
+  /**
+   * @brief Load images from the given path and set to the image batch.
+   * @todo if images are greater than certain size, automatic resize
+   * @param path The path to the directory containing images.
+   * @param resize Flag to resize the images.
+   */
   void load_images(const std::string &path, bool resize) {
     this->m_path = path;
     this->m_resize = resize;
@@ -38,14 +56,20 @@ public:
   };
 
 public:
-  cv::Mat stitch() {
+  /**
+   * @brief Stitch the image batch.
+   * @return The stitched image.
+   */
+  types::Image stitch() {
     this->stitch_images();
     return this->m_stitched_image;
   }
 
-public:
+private:
+  /**
+   * @brief Load images from the given path and stored in a batch of images.
+   */
   void load_images() {
-
     // Check if the directory exists
     if (!std::filesystem::exists(this->m_path) ||
         !std::filesystem::is_directory(this->m_path)) {
@@ -68,10 +92,7 @@ public:
 
     // Load and optionally resize images
     for (const auto &image_path : image_paths) {
-      cv::Mat image = cv::imread(image_path);
-      if (image.empty()) {
-        throw std::runtime_error("Failed to load image: " + image_path);
-      }
+      types::Image image = utils::read_image(image_path);
       if (this->m_resize) {
         cv::resize(image, image, cv::Size(image.cols / 4, image.rows / 4));
       }
@@ -79,54 +100,60 @@ public:
     }
   }
 
-public:
+private:
+  /**
+   * @brief Stitch the images.
+   * @details The images are split into two halves. The left images are
+   * stitched together. The right images are stitched together. Finally, the
+   * left and right panoramas are stitched together to create the final
+   * panorama.
+   */
   void stitch_images() {
-    size_t n = (this->m_images.size() + 1) / 2;
-    std::vector<cv::Mat> left(this->m_images.begin(),
-                              this->m_images.begin() + n);
-    std::vector<cv::Mat> right(this->m_images.begin() + n - 1,
-                               this->m_images.end());
+    size_t image_idx_mid = (this->m_images.size() + 1) / 2;
+    types::ImageBatch left(this->m_images.begin(),
+                           this->m_images.begin() + image_idx_mid);
+    types::ImageBatch right(this->m_images.begin() + image_idx_mid - 1,
+                            this->m_images.end());
     std::reverse(right.begin(), right.end());
 
     is::blend::ImageBlending blending;
 
     // Stitch left images
     while (left.size() > 1) {
-      cv::Mat dst_img = left.back();
+      types::Image dst_img = left.back();
       left.pop_back();
-      cv::Mat src_img = left.back();
+      types::Image src_img = left.back();
       left.pop_back();
-      cv::Mat left_pano = blending.warp_images(src_img, dst_img);
+      types::Image left_pano = blending.warp_images(src_img, dst_img);
       left_pano.convertTo(left_pano, CV_8U);
       left.push_back(left_pano);
     }
 
     // Stitch right images
     while (right.size() > 1) {
-      cv::Mat dst_img = right.back();
+      types::Image dst_img = right.back();
       right.pop_back();
-      cv::Mat src_img = right.back();
+      types::Image src_img = right.back();
       right.pop_back();
-      cv::Mat right_pano = blending.warp_images(src_img, dst_img);
+      types::Image right_pano = blending.warp_images(src_img, dst_img);
       right_pano.convertTo(right_pano, CV_8U);
       right.push_back(right_pano);
     }
 
     // Final stitch of left and right panoramas
-    cv::Mat final_pano;
+    types::Image final_pano;
     if (right.back().cols >= left.back().cols) {
       final_pano = blending.warp_images(left.back(), right.back());
     } else {
       final_pano = blending.warp_images(right.back(), left.back());
     }
     this->m_stitched_image = final_pano;
-    //    return final_pano;
   }
 
 private:
-  bool m_resize;
+  bool m_resize{};
   std::string m_path;
-  std::vector<cv::Mat> m_images;
+  types::ImageBatch m_images;
   types::Image m_stitched_image;
 };
 } // namespace is::stitch
