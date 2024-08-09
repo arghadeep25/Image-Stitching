292ec4b9db7ffe014f55e8c185b13c40d97faec2
Arghadeep Mazumder <33260290+arghadeep25@users.noreply.github.com>
2024-08-09 15:21:45 +0200

    command line argument feature added

--- src/image_stitching.cpp
+++ src/image_stitching.cpp
@@ -5,15 +5,17 @@
 #include <utils/image_visualizer.hpp>
 #include <utils/scoped_timer.hpp>
 
-int main() {
+int main(int argc, char *argv[]) {
   std::cout << "OpenCV Version: " << CV_VERSION << std::endl;
   cv::Mat pano;
+  std::string input_path = argc > 1 ? argv[1] : "../data/berlin";
+  std::string output_path = argc > 2 ? argv[2] : "../results/berlin.jpg";
   {
     ScopedTimer timer;
     is::stitch::Stitch stitch;
-    stitch.load_images("../data/berlin", false);
+    stitch.load_images(input_path, false);
     pano = stitch.stitch();
   }
   is::vis::display(pano, "Panorama");
-  cv::imwrite("../results/berlin.jpg", pano);
+  cv::imwrite(output_path, pano);
 }
