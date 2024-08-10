d3fdc681b7d7ba66b8694ade3a6f6b501c93ecc7
Arghadeep Mazumder <33260290+arghadeep25@users.noreply.github.com>
2024-08-10 08:44:45 +0200

    scoped timer header description added

--- include/utils/scoped_timer.hpp
+++ include/utils/scoped_timer.hpp
@@ -1,6 +1,10 @@
-//
-// Created by arghadeep on 29.07.24.
-//
+/**
+ * @file scoped_timer.hpp
+ * @details Header to get the execution time of a block of code.
+ * @author Arghadeep Mazumder
+ * @version 0.1.0
+ * @copyright -
+ */
 
 #ifndef IMAGE_STITCHING_SCOPED_TIMER_HPP
 #define IMAGE_STITCHING_SCOPED_TIMER_HPP
@@ -14,7 +18,7 @@ struct ScopedTimer {
   ScopedTimer() : start(Clock::now()) {}
   ~ScopedTimer() {
     const auto end = Clock::now();
-    std::cout << std::chrono::duration<double>(end - start).count() << " seconds";
+    std::cout << std::chrono::duration<double>(end - start).count() << " seconds" << std::endl;
   }
 };
 
